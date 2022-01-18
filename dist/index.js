92a07b9486136a506a0d21048dc735efbeafdf88
yagobrandao <yagobrandao20@gmail.com>
2022-01-18 17:41:19 -0300

    fix: users

--- dist/index.js
+++ dist/index.js
@@ -11565,8 +11565,8 @@ async function createGMUD(){
         requestTypeId: core.getInput('request-type-id'),
         id_card_issue: keyJira,
         service: [{"id" : "ari:cloud:graph::service/e27f7002-0a2f-11eb-98e1-0a77f3f45304/907c246a-0f3a-11ec-8fa8-0abe3f4a6601"}],
-        tecnical_approval: core.getInput('tecnical-approval'),
-        business_approval: core.getInput('business-approval'),
+        tecnical_approval: 'yago.brandao@madeiramadeira.com.br',
+        business_approval: 'vitor.nunes@madeiramadeira.com.br',
         url:  core.getInput('url-pull-request')
     }
 
@@ -11574,7 +11574,7 @@ async function createGMUD(){
         await axios.post(url_gmud, body,
             {
                 headers: {
-                  'authorization': core.getInput('basic_auth'),
+                  'Authorization': core.getInput('basic_auth'),
                   'apikey': core.getInput('api-key'),
                   'auth_github': core.getInput('auth-github'),
                   'Content-Type': 'application/json'
